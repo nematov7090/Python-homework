b8443bdd14600499f7ecf4e8808258ccaba82fd0
nematov7090 <is.shohrux@gmail.com>
2025-04-11 17:47:31 +0500

    Add files via upload

---
+++ lesson-1/homework/lesson-1.py
@@ -0,0 +1,99 @@
+
+# a = 1
+# b = 3
+# print("a + b = ",a +b )
+
+# a = "Hello world"
+# print(a)  
+
+# my_first_number=5 
+# my_second_number=10
+# print(my_first_number + my_second_number)
+
+# len = 5
+# my_second_number = 10
+# print(len + my_second_number)
+
+#######################
+#Data Type - Ma'lumot toifasi    
+
+# print(type(1)) # int - Integer - butun son
+
+# print(type(1.5))  # float - haqiqiy son
+
+# print(type('Hello')) # str -String -So'z 
+
+# print(type(true) bool
+
+# printe(type(False))  #bool
+
+# print(type(2 + 3j)) # complex
+
+##########################
+
+#NUMERIC - data type
+
+# print("Apple" * 5)
+
+# a=5.4
+# b=6
+# a+b
+# a-b 
+# a / b
+# print(a / b)
+# print(a // b) # // butun bo'lish
+# print(a % b) # % qoldiq
+
+# print(2 * 3)
+# print(2 ** 3)
+
+# a = 20
+# b = 6
+# print(divmod(a, b))  
+# x, y = divmod(a, b)
+# print('x =', x)
+# print('y =', y)
+
+# a = 6
+# b = 7
+# c = a + b
+# print("a + b =", c)
+
+# a = float(input("a= "))
+# b = float(input("b= "))
+# c = a + b
+# print("a + b =", c)
+# print(type(a))
+# a = int(a)
+# b = int(b)
+# print(type(a))
+# name = input("What is your name? ")
+# a=input()
+# print(a)
+
+##########################
+
+# name1 = "Adam" 
+# name2 = "Johnatan"    
+# print(f"{name1} - first name")
+# print(f"{name2} - second name")
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
